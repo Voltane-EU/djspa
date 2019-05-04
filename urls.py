4102b9c04fae9c66f5480c53dffac2b365b9ef6f
Manuel Stingl <manuel@stingl.st>
2019-05-04 21:38:19 +0200

    Use IndexView with page view class together for direct url call

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.get_page_view, name='index'),
     path('get_page/<page>', views.get_page, name='get_page'),
-    path('<page>/<subpage>', views.IndexView.as_view(), name='subpage_catchall'),
-    path('<page>', views.IndexView.as_view(), name='page_catchall'),
+    path('<page>/<subpage>', views.get_page_view, name='subpage_catchall'),
+    path('<page>', views.get_page_view, name='page_catchall'),
 ]
--- views.py
+++ views.py
@@ -51,6 +51,9 @@ def get_page(request, page):
     except TemplateDoesNotExist:
         return HttpResponseServerError()
 
+def get_page_view(request, page='index'):
+    return type("PageView", (IndexView, _pages[page],), {})(request=request).get(request)
+
 class IndexView(BaseView):
     template_name = 'index.html'
 
